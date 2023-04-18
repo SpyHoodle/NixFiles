1fc9aa10015ff2bf2756b4a9a0b1dd44bda52225
Madeleine <maddie@spyhoodle.me>
2023-04-18 07:02:51 +0100

    syncthing fixes and updates

--- systems/mdesktop/syncthing.nix
+++ systems/mdesktop/syncthing.nix
@@ -9,18 +9,31 @@
     configDir = "/home/${username}/.config/syncthing";
     guiAddress = "0.0.0.0:8384";
     overrideFolders = true;
+    devices = {
+      "M.Phone.Pixel" = { id = "WMGWVOU-DFZQSZO-46XQJQA-Q5XD5ZB-NDXK5SP-LXNAADH-Z2KJN4P-4P4UXA5"; };
+      "Lambda" = { id = "ZYNSFWR-F3ZNDDT-66TSJWB-PRP3KQK-IWTTABU-GGWT5DL-RWC7VAE-LI5AXQQ"; };
+    };
     folders = {
       "Documents" = {
         path = "/home/${username}/Documents";
+        devices = [ "M.Phone.Pixel" ];
       };
       "Pictures" = {
         path = "/home/${username}/Pictures";
+        devices = [ "M.Phone.Pixel" ];
       };
       "Videos" = {
         path = "/home/${username}/Videos";
+        devices = [ "M.Phone.Pixel" ];
       };
       "Music" = {
         path = "/home/${username}/Music";
+        devices = [ "M.Phone.Pixel" ];
+      };
+      "Android Camera" = {
+        id = "pixel_7_pro_2qyx-photos";
+        path = "/home/${username}/Pictures/DCIM";
+        devices = [ "M.Phone.Pixel" ];
       };
     };
   };
