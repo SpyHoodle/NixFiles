c4a15b2aa15c2072bfb8e9b64da32e233102ac27
Madeleine <maddie@spyhoodle.me>
2023-05-26 19:01:54 +0100

    fix razer chroma hdk

--- maddie/nixos/openrgb.nix
+++ maddie/nixos/openrgb.nix
@@ -9,6 +9,9 @@
     # Set RGB perhipherals to white
     ${pkgs.openrgb}/bin/openrgb -c white -m static &
 
+    # Set Razer Chroma HDK to white
+    ${pkgs.openrgb}/bin/openrgb --device 1 -c white &
+
     # Start my razer keyboard script
     ${pkgs.python3Full}/bin/python ${config.home.homeDirectory}/Documents/Code/Razer/system.py &
   '';
